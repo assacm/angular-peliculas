6b4c99697b047d228de4070594690cd5939e4fa7
assacm <alma_julissa@live.com.mx>
2024-12-11 12:20:57 -0700

    Formulario para creación de películas

--- src/app/peliculas/crear-pelicula/crear-pelicula.component.ts
+++ src/app/peliculas/crear-pelicula/crear-pelicula.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { PeliculaCreacionDTO } from '../peliculas';
+import { FormularioPeliculasComponent } from "../formulario-peliculas/formulario-peliculas.component";
 
 @Component({
   selector: 'app-crear-pelicula',
   standalone: true,
-  imports: [],
+  imports: [FormularioPeliculasComponent],
   templateUrl: './crear-pelicula.component.html',
   styleUrl: './crear-pelicula.component.css'
 })
 export class CrearPeliculaComponent {
+
+  guardarCambios(pelicula:PeliculaCreacionDTO){
+
+    console.log('creando pelicula',pelicula);
+  }
 
 }
--- src/app/peliculas/editar-pelicula/editar-pelicula.component.ts
+++ src/app/peliculas/editar-pelicula/editar-pelicula.component.ts
@@ -1,13 +1,28 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { PeliculaCreacionDTO, PeliculaDTO } from '../peliculas';
+import { FormularioPeliculasComponent } from "../formulario-peliculas/formulario-peliculas.component";
 
 @Component({
   selector: 'app-editar-pelicula',
   standalone: true,
-  imports: [],
+  imports: [FormularioPeliculasComponent],
   templateUrl: './editar-pelicula.component.html',
   styleUrl: './editar-pelicula.component.css'
 })
 export class EditarPeliculaComponent {
   @Input({transform:numberAttribute})
   id!:number;
+
+  pelicula:PeliculaDTO ={
+    id:1,
+    titulo:'Spider-Man',
+    trailer:'ABC',
+    fechaLanzamiento: new Date('2018-07-25'),
+    poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832'
+  }
+
+  guardarCambios(pelicula:PeliculaCreacionDTO){
+    console.log('editando película', pelicula);
+  }
+
 }
